672f03e5965f7390bf12742d0a440ee2b23c85c0
benyogit <ben14687@gmail.com>
2020-03-18 04:34:05 +0200

    Undo changes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dbConnect = require("./config/db");
 const app = express();
+const path = require('path');
 
 const usersRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
@@ -30,6 +31,15 @@ app.use("/api/users", usersRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postsRoutes);
 app.use("/api/profile", profileRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
