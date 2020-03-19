4d2766d6fdd4233698cb82e2831ce04838d50c78
benyogit <ben14687@gmail.com>
2020-03-19 19:36:02 +0200

    Edit dotenv.config

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const dbConnect = require("./config/db");
 const app = express();
 const path = require('path');
 const dotenv= require('dotenv');
 dotenv.config();
+const dbConnect = require("./config/db");
 const usersRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 const postsRoutes = require("./routes/posts");
