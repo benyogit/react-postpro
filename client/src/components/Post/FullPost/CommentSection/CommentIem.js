149072fa70b6af7cf4cc3b32b6756d735b7b392a
benyogit <ben14687@gmail.com>
2020-05-30 20:27:59 +0300

    Styling comment section

--- client/src/components/Post/FullPost/CommentSection/CommentIem.js
+++ client/src/components/Post/FullPost/CommentSection/CommentIem.js
@@ -5,35 +5,35 @@ import Moment from "react-moment";
 const CommentItem = props => {
   let btnDelete ;
   if (props.user === props.comment.user) {
-    btnDelete = <button  style={{ float: "right" }} onClick={props.deletion}><i className="far fa-trash-alt"></i></button>;
+    btnDelete = <button  onClick={props.deletion}><i className="far fa-trash-alt"></i></button>;
   } else {
     btnDelete =null;
   }
 
   return (
-    <div className="post bg-white p-1 my-1">
+    <div className="card ">
       <div >
         <Link to={`/profile/`}>
           <img className="round-img" alt="" />
         </Link>
       </div>
 
-      <div >
-        <p className="post-date" style={{ float: "right" }}>
+      <div className="card-header">
+        <p  style={{ float: "right" }}>
           Posted on  <Moment format="YYYY/MM/DD hh:mm">{props.comment.date}</Moment>
         </p>
         <Link to={`/profile/`}>
           <h4>{props.comment.name}</h4>
         </Link>
-        {btnDelete}
+        
       </div>
-      <div>
-        <p  className="my-1" style={{ float: "right" }}>
+      <div className="card-body">
+        <p  >
           {props.comment.text}
         </p>
-        
-         
-        
+      </div>
+      <div className="card-footer">
+          {btnDelete}
       </div>
     </div>
   );
