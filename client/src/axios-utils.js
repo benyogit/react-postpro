aa40a6bf92a8e3d9a50c3ff329d6895ba187bef7
benyogit <ben14687@gmail.com>
2020-09-28 10:09:05 +0300

    Changing API url in NODE_ENV

--- client/src/axios-utils.js
+++ client/src/axios-utils.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+
+const baseUrl = process.env.NODE_ENV =='production' ?  '/api' :"http://localhost:5000/";
+
 const instance = axios.create({
-    baseURL: '/api'
+    baseURL: baseUrl
 });
 
 export default instance;
